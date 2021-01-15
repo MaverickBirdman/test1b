8e5712b464d3fd0a08fcbf2e7f42cc9e55c66cfd
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2021-01-15 08:41:28 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,18 @@
 #include <string>
 
 // tutaj klasa Artysta
+class Artysta	//podaj nazwe klasy
+{
+public:
+	Artysta():pseudonim("Anonim"), mls(0) {}
+	Artysta(const string& a, unsigned b) :pseudonim(a), mls(b) {}
+	void graj(std::ostream& o) const 
+	{
+		stream << pseudonim<<": "<<mls;
+	}
+	const string& getPseudonim(std::ostream& o) const { return pseudonim; }
+	unsigned getSluchacze() const { return mls; }
+private:
+	string pseudonim;
+	unsigned mls;	//miesieczna liczba sluchaczy
+};
